db7849014437d603bce00720d64fb1a0496468cc
Brandon Brasson <rocksong622@gmail.com>
2019-04-09 20:54:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,7 +28,7 @@ def valid?
   sum_one_three = @triangle_sides[0] + @triangle_sides[2]
   sum_two_three = @triangle_sides[1] + @triangle_sides[2]
 
-  if (@triangle_sides.none? {|side| side <= 0}) &&
+  if (@triangle_sides.none? {|side| side <= 0}) 
     (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
     return true
   else
@@ -59,3 +59,7 @@ end
 
       #else
       #  puts "The line is currently empty."
+
+
+
+

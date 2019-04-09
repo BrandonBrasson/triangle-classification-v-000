b129a06e734d1f578f389d1757c9787a1b43184c
Brandon Brasson <rocksong622@gmail.com>
2019-04-09 21:39:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,13 +24,7 @@ class Triangle
   #  end
 #  end
 
-def sum
-  if (@triangle_sides.sum? {|side| side <= 0}) &&(sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-    return true
-  else
-    return false
-  end
-end
+
 
 def kind
   if sum

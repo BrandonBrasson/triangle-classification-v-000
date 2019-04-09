9ba8c76741dd7ad82b7b8e56d449ea338b4cb5c5
Brandon Brasson <rocksong622@gmail.com>
2019-04-09 20:52:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,24 +11,49 @@ class Triangle
     @triangle_sides << side_3
   end
 
-  def sum(triangle_side)
+  #def sum(triangle_side)
 
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    end
+    #  if @triangle_sides.uniq.length == 1
+    #    return :equilateral
+    #  elsif @triangle_sides.uniq.length == 2
+      #  return :isosceles
+    #  else
+      #  return :scalene
+  #  #  end
+  #  end
+#  end
+
+def valid?
+  sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+  sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+  sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+
+  if (@triangle_sides.none? {|side| side <= 0}) &&
+    (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+    return true
+  else
+    return false
   end
-      #  triangle_side.each_with_index {|name, index|  print " #{index +1}. #{name}" }
-      #else
-      #  puts "The line is currently empty."
+end
 
-
-
+def kind
+  if valid?
+    if @triangle_sides.uniq.length == 1
+      return :equilateral
+    elsif @triangle_sides.uniq.length == 2
+      return :isosceles
+    else
+      return :scalene
+    end
+  
+end
+end
 
 class TriangleError < StandardError
 
 end
+
+      #  triangle_side.each_with_index {|name, index|  print " #{index +1}. #{name}" }
+
+      #else
+      #  puts "The line is currently empty."

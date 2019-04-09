28e2cf64f8b1c0ae58bbb1d1405742211d0027d1
Brandon Brasson <rocksong622@gmail.com>
2019-04-09 20:14:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+#class Triangle
+#attr_attr
+#attr_accessor :side_1, :side_2, :side_3,
+#attr_reader :equilateral, :isosceles, :scalene
+
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+  end
+
+  def sum(triangle_side)
+
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+  end
+      #  triangle_side.each_with_index {|name, index|  print " #{index +1}. #{name}" }
+      #else
+      #  puts "The line is currently empty."
+
+
+
+
+class TriangleError < StandardError
+
 end
